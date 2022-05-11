e3ec12cc5d4543f0d75fb95124016300cb3a6ab8
Muhammad Riandi <62460140+riandiuniska@users.noreply.github.com>
2022-05-11 13:18:09 +0700

    Update group_chat.php
    update tombol end session dan countdown

--- group_chat.php
+++ group_chat.php
@@ -226,99 +226,74 @@ echo "<input type='hidden' name='userId' id='userId' value='" . $_SESSION['id'] 
                 <p class="text-3xl text-dark-green font-semibold">Project Management - System Analyst</p>
             </div>
 
+            <!-- TOP -->
+            <div class="border rounded-lg border-gray-300 bg-white">
+                <!-- Countdown,-->
+                <div class="relative">
+                    <!-- Countdown -->
+                    <div class="absolute text-white right-[133px] top-[20px] bg-slate-500 py-2 px-4 rounded-2xl">
+                        <i class="mr-1 fas fa-clock"></i>
+                        <span class="countdown font-mono text-base">
+                            <span style="--value:00;"></span>:
+                            <span style="--value:59;"></span>:
+                            <span style="--value:50;"></span>
+                        </span>
+                    </div>
+                    <!-- End Session -->
+                    <?php
 
-            <div class="relative mb-7">
-                <!-- Countdown -->
-                <div class="absolute text-white right-[113px] bg-slate-500 py-2 px-4 rounded-2xl">
-                    <i class="mr-1 fas fa-clock"></i>
-                    <span class="countdown font-mono text-sm">
-                        <span style="--value:10;"></span>:
-                        <span style="--value:24;"></span>:
-                        <span style="--value:59;"></span>
-                    </span>
-                </div>
-                <!-- End Session -->
-                <?php
-                
-                if($_SESSION['id'] == 2){
-                   echo '<div class="absolute right-[0px]">
-                    <button class="py-2 pb-3 px-4 text-sm font-medium first-line:text-center text-white bg-yellow-600 rounded-2xl hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-200" type="button" data-modal-toggle="deleteSes">
+                    if ($_SESSION['id'] == 2) {
+                        echo '<div class="absolute right-[20px] top-[20px]">
+                    <button class="py-[10px] px-4 text-sm font-medium text-white bg-red-700 rounded-2xl hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-200" type="button" data-modal-toggle="deleteSes">
                         End Session 
                     </button>
                     </div>';
-                } else {
-                    
-                }
+                    } else {
+                    }
 
-                ?>
+                    ?>
 
+                    <div id="deleteSes" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+                        <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="deleteSes">
+                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                                <div class="p-6 text-center">
+                                    <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                    </svg>
+                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to end this Session?</h3>
 
+                                    <form action="" method="post">
+                                        <input type="text" name="idEnd" id="idEnd" value="" hidden>
+                                        <button data-modal-toggle="deleteSes" type="submit" name="endChat" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                            Yes, I'm sure
+                                        </button>
+                                    </form>
 
-                <div id="deleteSes" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-                    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="deleteSes">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                </svg>
-                            </button>
-                            <div class="p-6 text-center">
-                                <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to end this Session?</h3>
-
-                                <form action="" method="post">
-                                    <input type="text" name="idEnd" id="idEnd" value="" hidden>
-                                    <button data-modal-toggle="deleteSes" type="submit" name="endChat" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                        Yes, I'm sure
-                                    </button>
-                                </form>
-
-                                <button data-modal-toggle="deleteSes" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                    <button data-modal-toggle="deleteSes" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="absolute right-[0px]">
-                    <form action="" method="post">
-                        <input type="text" name="idEnd" id="idEnd" value="" hidden>
-                        <button type="submit" name="endChat" class="py-2 pb-3 px-4 text-sm font-medium first-line:text-center text-white bg-yellow-600 rounded-2xl hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-200">
-                            <p class="translate-y-0.5">End Session</p>
-                        </button>
-                    </form>
-                </div> -->
-            </div>
 
-            <div class="flex">
-                <!-- Search Chat -->
-                <div class="w-1/3 mr-6 mt-10 rounded-xl">
-                    <!-- <form>
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-                        <div class="relative">
-                            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                </svg>
-                            </div>
-                            <input type="search" id="default-search" class="block p-3 pl-10 w-full text-sm text-gray-900  rounded-xl border border-gray-300 focus:ring-[#ddb07f] focus:border-[#ddb07f]" name="search" placeholder="Search Message" required>
-                        </div>
-                    </form> -->
-                </div>
-                <!-- User Konsultasi -->
-                <div class="w-2/3 border rounded-lg border-gray-300 bg-white">
-                    <div>
-                        <div class="flex gap-x-4 py-3 px-7">
-                            <!-- <img class="w-14" src="Img/icons/default_profile.svg" alt="Profile Image"> -->
-                            <div class="">
-                                <p class="flex py-4 text-dark-green text-base font-bold "> Bimbingan
-                                </p>
 
-                            </div>
+                <!-- Name Rooms -->
+                <div>
+                    <div class="flex gap-x-4 py-3 px-7">
+                        <!-- <img class="w-14" src="Img/icons/default_profile.svg" alt="Profile Image"> -->
+                        <div class="">
+                            <p class="flex py-4 text-dark-green font-bold text-lg"> BIMBINGAN
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
+
 
             <div class="flex">
                 <!-- List Chat -->
